a3808efd972da55973670fe973a31126d68ef898
Sidharth-KV <96193292+Sidharth-KV@users.noreply.github.com>
2024-11-16 21:52:51 +0530

    2nd commit

--- assets/js/cursor.js
+++ assets/js/cursor.js
@@ -1,11 +1,4 @@
-function AddAnimatedCircle() {
-  const circle = document.getElementById('circleId');
-  document.addEventListener('mousemove', (e) => {
-    requestAnimationFrame(() => {
-      circle.style.left = `${e.clientX}px`;
-      circle.style.top = `${e.clientY}px`;
-    });
-  });
+function takeUSN() {
+  var input = document.getElementById("userInput");
+  alert(input);
 }
-
-AddAnimatedCircle();
